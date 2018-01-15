1e80275e1f5ade74d145386f3e03cd55f66c5c54
Djuuu <julien@tardot.net>
2018-01-15 14:26:39 +0100

    env conf + ffshared network

--- generate-certs.sh
+++ generate-certs.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./certs/dev.key -out ./certs/dev.crt
+# cd to deploy script directory
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# source environment file
+. ./.env
+
+
+# Generate cert
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./certs/${NGINX_PROXY_CERT_NAME}.key -out ./certs/${NGINX_PROXY_CERT_NAME}.crt
